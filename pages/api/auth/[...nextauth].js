360becc9720086948573a1e2c8ad38d0c5439480
Jasmeet Singh Arora <jasspn2014@gmail.com>
2021-05-21 17:47:57 +0530

    Removed Facebook Auth and Added Google Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,9 +3,11 @@ import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
     }),
   ],
 });
